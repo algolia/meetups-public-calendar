2f7c742753ede196ca2f12d9d2b65243f178f46b
Tim Carry <tim@pixelastic.com>
2025-10-29 21:40:44 +0100

    Fix lint

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,14 @@
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
 import config from 'aberlaas/configs/vite';
+
+// @vitejs/plugin-react uses the exports syntax in its package.json, which
+// eslint-plugin-import doesn't support
+// eslint-disable-next-line import/no-unresolved
+import vitePluginReact from '@vitejs/plugin-react';
 
 export default defineConfig({
   ...config,
-  plugins: [react()],
+  plugins: [vitePluginReact()],
   root: '.',
   build: {
     outDir: 'dist',
