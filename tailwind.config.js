6c305ec5c2f0b35e92cc8787b7dedbdb7a60aa95
Tim Carry <tim@pixelastic.com>
2025-11-20 13:07:47 +0100

    Downgrade to Tailwind v3

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./index.html', './src/**/*.{js,jsx}'],
+  theme: {
+    extend: {
+      scale: {
+        120: '1.2',
+      },
+    },
+  },
+  plugins: [],
+};
--- vite.config.js
+++ vite.config.js
@@ -4,13 +4,12 @@ import config from 'aberlaas/configs/vite';
 /* eslint-disable import/no-unresolved */
 // Those plugins uses the .exports syntax in their package.json, which isn't yet
 // supported by eslint-plugin-import
-import vitePluginTailwind from '@tailwindcss/vite';
 import vitePluginReact from '@vitejs/plugin-react';
 /* eslint-enable import/no-unresolved */
 
 export default defineConfig({
   ...config,
-  plugins: [vitePluginTailwind(), vitePluginReact()],
+  plugins: [vitePluginReact()],
   root: '.',
   build: {
     outDir: 'dist',
